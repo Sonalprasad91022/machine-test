e658f9e30c4fd307cebf6f3a775dc9549ae45393
Sonalprasad91022 <116148553+Sonalprasad91022@users.noreply.github.com>
2025-05-11 23:18:21 +0530

    Initial commit

---
+++ client/src/components/navbar.js
@@ -0,0 +1,15 @@
+// src/components/Layout.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-100">
+      {/* Navigation Bar */}
+      <nav className="bg-blue-500 p-4">
+      <div style={{color:'white'}}>Dashboard</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
